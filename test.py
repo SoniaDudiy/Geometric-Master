f08d1c2f0d54b6cca6d3196bf1cb3716d849d5bf
Sonia <116679495+SoniaDudiy@users.noreply.github.com>
2024-05-27 05:20:10 -0700

    second

---
+++ test.py
@@ -0,0 +1,26 @@
+import unittest
+from main import Square, Rectangle, Triangle, Circle
+
+class TestShapeCalculations(unittest.TestCase):
+    def test_square(self):
+        square = Square(5)
+        self.assertAlmostEqual(square.area(), 25)
+        self.assertAlmostEqual(square.perimeter(), 20)
+
+    def test_rectangle(self):
+        rectangle = Rectangle(4, 6)
+        self.assertAlmostEqual(rectangle.area(), 24)
+        self.assertAlmostEqual(rectangle.perimeter(), 20)
+
+    def test_triangle(self):
+        triangle = Triangle(3, 4)
+        self.assertAlmostEqual(triangle.area(), 6)
+        self.assertAlmostEqual(triangle.perimeter(), 12)
+
+    def test_circle(self):
+        circle = Circle(3)
+        self.assertAlmostEqual(circle.area(), 9 * 3.14159, places=4)
+        self.assertAlmostEqual(circle.perimeter(), 6 * 3.14159, places=4)
+
+if __name__ == '__main__':
+    unittest.main()
